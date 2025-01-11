19939cbe0e5cea53d19380aba5b5a3289729763f
Ednilson Rossi <ednilsonrossi@gmail.com>
2025-01-11 18:51:14 -0300

    configuração do DB, Helper, Dao, Model e activity. Salvar um dado com texto no banco

--- app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/ui/main/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/ui/main/MainActivity.kt
@@ -5,17 +5,48 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import br.edu.ifsp.dmo1.exemplosqlite.R
+import br.edu.ifsp.dmo1.exemplosqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: MeuDadoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        setupRecyclerView()
+        setupObservers()
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.buttonAdd.setOnClickListener {
+            viewModel.addDado("Novo texto")
         }
     }
+
+    private fun setupObservers() {
+        viewModel.dados.observe(this, Observer {
+            adapter.updateDados(it)
+        })
+    }
+
+    private fun setupRecyclerView() {
+        adapter = MeuDadoAdapter(mutableListOf())
+        binding.listDados.adapter = adapter
+        binding.listDados.layoutManager = LinearLayoutManager(this)
+    }
+
+
 }
